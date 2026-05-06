27ed93d2c18551d1e0729bf0475948d069305f47
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-06 20:10:56 +0100

    Create table for skills that will be used in volunteer profiles and events

--- back-end/database/__init__.py
+++ back-end/database/__init__.py
@@ -3,6 +3,7 @@ import os
 from sqlmodel import Session, create_engine, SQLModel, select
 from database.models.status import Status
 from database.models.user import User # noqa: F401
+from database.models.skill import Skill
 
 # Load environment variables from .env file
 load_dotenv()
@@ -33,5 +34,44 @@ def seed_statuses():
             Status(name="Active"),
             Status(name="Inactive"),
             Status(name="Locked")
+        ])
+        session.commit()
+
+def seed_skills():
+
+    with Session(engine) as session:
+        # Check if populated table already exists
+        if session.exec(select(Skill)).first():
+            return
+        session.add_all([
+            Skill(name="Driving (own vehicle)"),
+            Skill(name="Cooking / food preparation"),
+            Skill(name="Gardening / outdoor work"),
+            Skill(name="Manual labour (moving, lifting, sorting)"),
+            Skill(name="Handyman / basic repairs"),
+            Skill(name="Cleaning"),
+            Skill(name="Childcare experience"),
+            Skill(name="Elderly care experience"),
+            Skill(name="Working with people with disabilities"),
+            Skill(name="Tutoring / teaching"),
+            Skill(name="Mentoring"),
+            Skill(name="Active listening / companionship"),
+            Skill(name="IT / digital literacy support"),
+            Skill(name="Administrative / office work"),
+            Skill(name="Bookkeeping / accounting"),
+            Skill(name="Legal knowledge"),
+            Skill(name="Healthcare / medical background"),
+            Skill(name="First aid certifiied"),
+            Skill(name="Translation / interpretation"),
+            Skill(name="Writing /editing"),
+            Skill(name="Social media / communications"),
+            Skill(name="Photography / videography"),
+            Skill(name="Graphic design"),
+            Skill(name="Event coordination"),
+            Skill(name="Fundraising"),
+            Skill(name="Public speaking"),
+            Skill(name="Music"),
+            Skill(name="Arts and crafts"),
+            Skill(name="Sports coaching"), 
         ])
         session.commit()
