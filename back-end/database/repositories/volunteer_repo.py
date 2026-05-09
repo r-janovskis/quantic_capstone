e78fa025fb813f478165c95d8ac21a96483ae474
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-09 21:34:38 +0100

    Add functions to create entries in junction tables for volunteer skills, interests and languages

--- back-end/database/repositories/volunteer_repo.py
+++ back-end/database/repositories/volunteer_repo.py
@@ -12,8 +12,29 @@ def create_volunteer(session: Session, volunteer: Volunteer) -> Volunteer:
     session.refresh(volunteer)
     return volunteer
 
+def create_volunteer_skills(session: Session,volunteer_id: int, skills: list[int]) -> None:
+    session.add_all([
+        VolunteerSkill(volunteer_id=volunteer_id, skill_id=skill_id) 
+        for skill_id in skills
+    ])
+    session.commit()
+
+def create_volunteer_interests(session: Session, volunteer_id: int, interests: list[int]) -> None:
+    session.add_all([
+        VolunteerInterest(volunteer_id=volunteer_id, interest_id=interest_id) 
+        for interest_id in interests
+    ])
+    session.commit()
+
+def create_volunteer_languages(session: Session, volunteer_id: int, languages: list[int]) -> None:
+    session.add_all([
+        VolunteerLanguage(volunteer_id=volunteer_id, language_id=language_id)
+        for language_id in languages
+    ])
+    session.commit()
+
 
 def get_all_volunteers(session: Session) -> list[Volunteer]:
     statement = select(Volunteer)
     return list(session.exec(statement).all())
- 
+
