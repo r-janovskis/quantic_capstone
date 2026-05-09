98bffcb6a80d33b278b144a633e21457c7b3372c
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-09 21:45:36 +0100

    Correct typo from __tablename= ... to __tablename__: ...

--- back-end/database/models/volunteer_interest.py
+++ back-end/database/models/volunteer_interest.py
@@ -2,6 +2,6 @@ from sqlmodel import SQLModel, Field
 from typing import ClassVar
 
 class VolunteerInterest(SQLModel, table=True):
-    __tablename__ = ClassVar[str] = "volunteers_interests" #type: ignore[assignment]
+    __tablename__: ClassVar[str] = "volunteers_interests" #type: ignore[assignment]
     volunteer_id: int = Field(foreign_key="volunteers.id", primary_key=True)
-    interest_id: int = Field(foreign_key="interests.id", primary_key=True)
+    interest_id: int = Field(foreign_key="skills.id", primary_key=True)
--- back-end/database/models/volunteer_skill.py
+++ back-end/database/models/volunteer_skill.py
@@ -2,6 +2,6 @@ from sqlmodel import SQLModel, Field
 from typing import ClassVar
 
 class VolunteerSkill(SQLModel, table=True):
-    __tablename__ = ClassVar[str] = "volunteers_skills" #type: ignore[assignment]
+    __tablename__: ClassVar[str] = "volunteers_skills" #type: ignore[assignment]
     volunteer_id: int = Field(foreign_key="volunteers.id", primary_key=True)
     skill_id: int = Field(foreign_key="skills.id", primary_key=True)
