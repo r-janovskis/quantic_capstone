a7212da367f4628eab827965bc7507c7cb5721e2
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-12 18:25:16 +0100

    Correct function name to get_all_time_periods (had stayed old one from copy/paste)

--- back-end/database/repositories/time_period_repo.py
+++ back-end/database/repositories/time_period_repo.py
@@ -2,6 +2,6 @@ from sqlmodel import Session, select
 from database.models.time_period import TimePeriod
 
 
-def get_all_days(session: Session) -> list[TimePeriod]:
+def get_all_time_periods(session: Session) -> list[TimePeriod]:
     statement = select(TimePeriod)
     return list(session.exec(statement).all())
