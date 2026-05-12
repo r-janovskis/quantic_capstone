695d4f9c917f610f049900d0322810082757ef75
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-12 18:52:50 +0100

    Expand time_periods model to have columns for start and end times

--- back-end/database/models/time_period.py
+++ back-end/database/models/time_period.py
@@ -1,12 +1,17 @@
 from sqlmodel import SQLModel, Field
+from sqlalchemy import CheckConstraint
 from typing import Optional, ClassVar
+from datetime import time
 
 
 class TimePeriodBase(SQLModel):
     name: str
+    start_time: time
+    end_time: time
 
 class TimePeriod(TimePeriodBase, table=True):
     __tablename__: ClassVar[str] = "time_periods" #type: ignore[assignment]
+    __table_args__ = (CheckConstraint("end_time > start_time", name="ck_time_period_end_after_start"),)
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(unique=True)
 
