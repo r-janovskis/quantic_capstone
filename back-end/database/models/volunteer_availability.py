9bb740e0ecf3454f7e0553449874386144a3c9d9
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-12 21:10:59 +0100

    Update VolunteerAvailability so include table=True in it's definition

--- back-end/database/models/volunteer_availability.py
+++ back-end/database/models/volunteer_availability.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel, Field
 from typing import ClassVar
 
 
-class VolunteerAvailability(SQLModel):
+class VolunteerAvailability(SQLModel, table=True):
     __tablename__: ClassVar[str] = "volunteers_availability" #type: ignore[assignment]
     volunteer_id: int = Field(foreign_key="volunteers.id", primary_key=True)
     day_id: int = Field(foreign_key="days.id", primary_key=True)
