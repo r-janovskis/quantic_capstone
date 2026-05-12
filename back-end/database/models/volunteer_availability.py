fb8dfb1ed8cb7f87c2a61f3cdbfde545e6c83722
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-12 19:52:47 +0100

    Create model for volunteers_availability table where we will track volunteer availability

---
+++ back-end/database/models/volunteer_availability.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, Field
+from typing import ClassVar
+
+
+class VolunteerAvailability(SQLModel):
+    __tablename__: ClassVar[str] = "volunteers_availability" #type: ignore[assignment]
+    volunteer_id: int = Field(foreign_key="volunteers.id", primary_key=True)
+    day_id: int = Field(foreign_key="days.id", primary_key=True)
+    time_period_id: int = Field(foreign_key="time_periods.id", primary_key=True)
