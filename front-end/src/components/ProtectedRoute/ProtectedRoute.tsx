8cc382086166f565754e90ce09659899b9ab6759
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-14 22:19:31 +0100

    Update component to support role based access

--- front-end/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ front-end/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,8 +1,15 @@
 import { useState, useEffect } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import authServices from "../../services/authAPI";
+import { getTokenRole } from "../../utils/token";
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({
+  requiredRole,
+  requireNoRole,
+}: {
+  requiredRole?: string;
+  requireNoRole?: boolean;
+}) => {
   const [verified, setVerified] = useState<boolean | null>(null);
 
   useEffect(() => {
@@ -27,7 +34,23 @@ const ProtectedRoute = () => {
   if (verified === null) {
     return "Loadning...";
   }
-  return verified ? <Outlet /> : <Navigate to="/auth/login" replace />;
+  if (!verified) {
+    <Navigate to="/auth/login" replace />;
+  }
+  if (requireNoRole) {
+    const token = localStorage.getItem("token")!;
+    const role = getTokenRole(token);
+    if (role === "volunteer") {
+      return <Navigate to="/volunteer/dashboard" replace />;
+    }
+  }
+  if (requiredRole) {
+    const token = localStorage.getItem("token")!;
+    if (getTokenRole(token) !== requiredRole) {
+      return <Navigate to="/" replace />;
+    }
+  }
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
