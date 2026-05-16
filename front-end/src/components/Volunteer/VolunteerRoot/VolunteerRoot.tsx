032a464f2294fc70b9afd5e5012a9c7845227bb2
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-16 23:33:44 +0100

    Wrap component in HeaderContext so we could facitilate header re-render when necessary

--- front-end/src/components/Volunteer/VolunteerRoot/VolunteerRoot.tsx
+++ front-end/src/components/Volunteer/VolunteerRoot/VolunteerRoot.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../headers/Header/Header";
+import { HeaderProvider } from "../../../context/HeaderContext";
 
 function VolunteerRoot() {
   return (
-    <>
+    <HeaderProvider>
       <Header />
       <Outlet />
-    </>
+    </HeaderProvider>
   );
 }
 
