4e3bf5a9b017b0e98c731d2ce8f1c8fdcf581126
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-16 20:27:24 +0100

    Add some dummy content on the page as placeholders for upcomming content

--- front-end/src/components/Volunteer/VolunteerDashboard/VolunteerDashboard.tsx
+++ front-end/src/components/Volunteer/VolunteerDashboard/VolunteerDashboard.tsx
@@ -4,8 +4,22 @@ function VolunteerDashboard() {
   return (
     <div>
       <h1>Volunteer Dashboard</h1>
-      <NavLink to="/volunteer/me">My Profile</NavLink>
-      <NavLink to="/">Home page</NavLink>
+      <section>
+        <h2> My upcoming events/shifts</h2>
+        <p> A list or cards of upcoming events I have signed up to</p>
+        <p> Still to do</p>
+      </section>
+      <section>
+        <h2> My event history</h2>
+        <p> You have not participated at any events yet...</p>
+        <p> Still to do</p>
+      </section>
+      <section>
+        <h2> What else could we put in here?</h2>
+        <p> You have not participated at any events yet...</p>
+        <p> Still to do</p>
+        <NavLink to="/volunteer/me">My Profile</NavLink>
+      </section>
     </div>
   );
 }
