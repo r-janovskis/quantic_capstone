0e2038ceb60037d4efa54c42f79869a6e7934f45
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-16 23:28:04 +0100

    Add type for retrieved data from API endpoint GET /volunteer/me

--- front-end/src/types.ts
+++ front-end/src/types.ts
@@ -3,4 +3,21 @@ type Option = {
   label: string;
 };
 
-export type { Option };
+type VolunteerProfileData = {
+  avatar_url: string | null;
+  display_name: string;
+  first_name: string;
+  last_name: string;
+  phone: string;
+  date_of_birth: string;
+  area: string;
+  country_id: number;
+  shirt_size_id: number;
+  bio: string;
+  skills: Option[];
+  interests: Option[];
+  languages: Option[];
+  availability: { day_id: number; time_period_id: number }[];
+};
+
+export type { Option, VolunteerProfileData };
