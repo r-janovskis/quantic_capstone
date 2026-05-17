12c9903009240362b3ebd346ad4046e6244332aa
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-17 16:19:53 +0100

    Remove FloatingLabels as they provided redundant information and made select fields look bloated

--- front-end/src/components/Volunteer/VolunteerAvailabilitySlot/DayPicker.tsx
+++ front-end/src/components/Volunteer/VolunteerAvailabilitySlot/DayPicker.tsx
@@ -1,4 +1,3 @@
-import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 
 import type { Option } from "../../../types";
@@ -13,24 +12,21 @@ const DayPicker = ({
   options: Option[];
 }) => {
   return (
-    <FloatingLabel className="day_picker" label="Available day/-s">
-      <Form.Select
-        value={value}
-        onChange={(event) =>
-          onChange(
-            event.target.value === "" ? null : Number(event.target.value)
-          )
-        }
-        aria-label="Day availability selector"
-      >
-        <option value="">Select day</option>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </Form.Select>
-    </FloatingLabel>
+    <Form.Select
+      className="day_picker"
+      value={value}
+      onChange={(event) =>
+        onChange(event.target.value === "" ? null : Number(event.target.value))
+      }
+      aria-label="Day availability selector"
+    >
+      <option value="">Select day</option>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </Form.Select>
   );
 };
 
--- front-end/src/components/Volunteer/VolunteerAvailabilitySlot/TimePeriodPicker.tsx
+++ front-end/src/components/Volunteer/VolunteerAvailabilitySlot/TimePeriodPicker.tsx
@@ -1,4 +1,3 @@
-import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 
 import type { Option } from "../../../types";
@@ -13,24 +12,21 @@ const TimePeriodPicker = ({
   onChange: (value: number | null) => void;
 }) => {
   return (
-    <FloatingLabel className="time_period_picker" label="Time period">
-      <Form.Select
-        value={value}
-        onChange={(event) =>
-          onChange(
-            event.target.value === "" ? null : Number(event.target.value)
-          )
-        }
-        aria-label="Time period selector"
-      >
-        <option value="">Select time period</option>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </Form.Select>
-    </FloatingLabel>
+    <Form.Select
+      className="time_period_picker"
+      value={value}
+      onChange={(event) =>
+        onChange(event.target.value === "" ? null : Number(event.target.value))
+      }
+      aria-label="Time period selector"
+    >
+      <option value="">Select time period</option>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </Form.Select>
   );
 };
 
