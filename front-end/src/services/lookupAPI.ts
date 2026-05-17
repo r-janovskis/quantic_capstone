92c3d777cc2d13dbc2f9d6826b5a49973fbeda74
Reinis Janovskis <reinis.janovskis@hotmail.com>
2026-05-17 21:30:27 +0100

    Refactor lookupAPI by replacing individual call to each lookup endpoint with factory function that return necessary data

--- front-end/src/services/lookupAPI.ts
+++ front-end/src/services/lookupAPI.ts
@@ -1,72 +1,31 @@
 import axios, { type AxiosResponse } from "axios";
 
 // Interface for responses for field values on profile/shift creation forms
+import type { LookupResponse } from "../types";
 
-interface LookupResponse {
-  id: number;
-  name: string;
-}
+type LookupEndpoint =
+  | "skills"
+  | "interests"
+  | "languages"
+  | "shirt_sizes"
+  | "countries"
+  | "days"
+  | "time_periods";
 
 // BASE_URL => "http://localhost:8000/lookup";
 const BASE_URL = `${import.meta.env.VITE_API_URL}/lookup`;
 
-const getSkills = async (): Promise<LookupResponse[]> => {
-  const endpoint = BASE_URL + "/skills";
-  const response: AxiosResponse<LookupResponse[]> = await axios.get(endpoint);
-
-  return response.data;
-};
-
-const getLanguages = async (): Promise<LookupResponse[]> => {
-  const endpoint = BASE_URL + "/languages";
-  const response: AxiosResponse<LookupResponse[]> = await axios.get(endpoint);
-
-  return response.data;
-};
-
-const getCountries = async (): Promise<LookupResponse[]> => {
-  const endpoint = BASE_URL + "/countries";
-  const response: AxiosResponse<LookupResponse[]> = await axios.get(endpoint);
-
-  return response.data;
-};
-
-const getShirtSizes = async (): Promise<LookupResponse[]> => {
-  const endpoint = BASE_URL + "/shirt_sizes";
-  const response: AxiosResponse<LookupResponse[]> = await axios.get(endpoint);
-
-  return response.data;
-};
-
-const getInterests = async (): Promise<LookupResponse[]> => {
-  const endpoint = BASE_URL + "/interests";
-  const response: AxiosResponse<LookupResponse[]> = await axios.get(endpoint);
-
-  return response.data;
-};
-
-const getDays = async (): Promise<LookupResponse[]> => {
-  const endpoint = BASE_URL + "/days";
-  const response: AxiosResponse<LookupResponse[]> = await axios.get(endpoint);
-
-  return response.data;
-};
-
-const getTimePeriods = async (): Promise<LookupResponse[]> => {
-  const endpoint = BASE_URL + "/time_periods";
+const getLookupData = async (
+  lookupData: LookupEndpoint
+): Promise<LookupResponse[]> => {
+  const endpoint = `${BASE_URL}/${lookupData}`;
   const response: AxiosResponse<LookupResponse[]> = await axios.get(endpoint);
 
   return response.data;
 };
 
 const lookupServices = {
-  getSkills,
-  getLanguages,
-  getCountries,
-  getShirtSizes,
-  getInterests,
-  getDays,
-  getTimePeriods,
+  getLookupData,
 };
 
 export default lookupServices;
